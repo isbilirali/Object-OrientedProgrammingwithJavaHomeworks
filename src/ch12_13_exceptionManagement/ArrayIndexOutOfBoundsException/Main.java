fc93ea1e2b5567d622cb27c372d7d1a7a2b0d0d3
isbilirali <isbilir1903@gmail.com>
2026-06-28 17:15:40 +0300

    updated.

---
+++ src/ch12_13_exceptionManagement/ArrayIndexOutOfBoundsException/Main.java
@@ -0,0 +1,40 @@
+package ch12_13_exceptionManagement.ArrayIndexOutOfBoundsException;
+
+import java.util.Scanner;
+
+/*
+1. Geçilen bir int dizisinin yine geçilen indisindeki elemanını geri
+döndüren int getElement(int[] array, int index)
+metodunu yazın. Bu metotta fırlatılabilecek
+ArrayIndexOutOfBoundsException sıra dışı durumunu yakalayın
+ve bu durumu belirtmek üzere metodun -1 döndürmesini sağlayın.
+ */
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arr = {100,200,300,400,500};
+
+        System.out.println("Which index you need from the array? : ");
+        int index  = scanner.nextInt();
+
+        System.out.println(getElement(arr,index));
+
+
+
+    }
+
+    private static int getElement(int[] arr, int index) {
+
+        try{
+            return arr[index];
+        } catch(ArrayIndexOutOfBoundsException e){
+            return -1111111111;
+        }
+    }
+
+
+}
